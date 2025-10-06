9337090dad02bb485c656efad73e2dcad2719b30
Pon157 <Vitechek.208@gmail.com>
2025-10-06 20:39:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -803,6 +803,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/main', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'main.html'));
+});
+
 app.get('/health', (req, res) => {
     res.json({ status: 'OK', timestamp: new Date().toISOString() });
 });
